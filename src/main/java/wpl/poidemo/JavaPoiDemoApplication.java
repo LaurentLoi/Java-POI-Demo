900c009046267880f750860515728e8180917b1e
Laurent Loiseau <lt.loiseau@gmail.com>
2021-04-06 10:57:45 +0200

    Added OOXML Dependency Added Excel BaseDemo class

--- src/main/java/wpl/poidemo/JavaPoiDemoApplication.java
+++ src/main/java/wpl/poidemo/JavaPoiDemoApplication.java
@@ -1,13 +1,12 @@
 package wpl.poidemo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaPoiDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaPoiDemoApplication.class, args);
+
     }
 
 }
