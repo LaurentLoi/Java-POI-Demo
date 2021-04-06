1bea7f0414f76035cce2578927b15b86b098a9c9
Laurent Loiseau <lt.loiseau@gmail.com>
2021-04-06 09:56:55 +0200

    project init

---
+++ src/main/java/wpl/poidemo/JavaPoiDemoApplication.java
@@ -0,0 +1,13 @@
+package wpl.poidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPoiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPoiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wpl/poidemo/JavaPoiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package wpl.poidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPoiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
